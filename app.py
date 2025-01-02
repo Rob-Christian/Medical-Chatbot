94d55f380d2e2f585dd3c70bdfc399ce36f93938
Rob Christian Caduyac <59250293+Rob-Christian@users.noreply.github.com>
2025-01-02 18:41:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -76,13 +76,13 @@ def initialize_graph():
     graph_builder.add_edge("generate", END)
     return graph_builder.compile(checkpointer=MemorySaver())
 
-new_thread_id = str(uuid.uuid4())
-config = {"configurable": {"thread_id": new_thread_id}}
 graph = initialize_graph()
 
 @app.route("/")
 def index():
     """Render the main interface for the chatbot."""
+    new_thread_id = str(uuid.uuid4())
+    config["configurable"]["thread_id"] = new_thread_id
     return render_template("index.html")
 
 @app.route("/new_chat", methods=["POST"])
